ff62cdcfadb13c39063cfc54736b395de42a8322
Kristen Couty <kristen.couty@gmail.com>
2024-07-15 00:51:47 +0200

    update: time & date display improvement, new date & time handling/calculation

--- src/commands/leaderboard.mjs
+++ src/commands/leaderboard.mjs
@@ -1,77 +1,93 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { getUser } from '../database.mjs';
-import { EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getUser } from "../database.mjs";
+import { EmbedBuilder } from "discord.js";
 import { Logtail } from "@logtail/node";
 
 const logtail = new Logtail(process.env.SOURCE_TOKEN);
 
 let COMMAND_DEFINITION = new SlashCommandBuilder()
-    .setName('leaderboard')
-    .setDescription('Replies with the leaderboard of the time spent in voice channels!');
+  .setName("leaderboard")
+  .setDescription(
+    "Replies with the leaderboard of the time spent in voice channels!",
+  );
 
 async function getLeaderboard(interaction) {
-    try {
-        const users = await getUser();
+  try {
+    const users = await getUser();
 
-        const date = new Date();
-        const startDate = parseInt(process.env.START_DATE)*1000;
-        let difference = Math.round((date.getTime() - startDate)/(1000*3600*24))
-        if (difference === 0) {
-            difference = 1;
-        }
-
-        const leaderboardEmbed = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(`${interaction.guild.name}'s leaderboard`)
-        .setThumbnail(interaction.guild.iconURL())
-        .setTimestamp()
-        .addFields(
-            users.map((user, index) => {
-
-                const hours = Math.floor(parseInt(user.time)/3600);
-                const minutes = Math.floor((parseInt(user.time)%3600)/60);
-                const seconds = parseInt(user.time)%60;
-                let time = "";
-
-                if (hours === 0) {
-                    time = minutes + 'min ' + seconds + 'sec';
-                } else {
-                    time = hours + 'h ' + minutes + 'min ' + seconds + 'sec';
-                }
-
-                const avgHours = Math.floor(parseInt(user.time)/3600/difference);
-                const avgMinutes = Math.floor((parseInt(user.time)%3600/difference)/60);
-                const avgSeconds = Math.floor((parseInt(user.time)/difference)%60);
-                let avgTime = "";
-
-                if (avgHours === 0) {
-                    avgTime = avgMinutes + 'min ' + avgSeconds + 'sec';
-                } else {
-                    avgTime = avgHours + 'h ' + avgMinutes + 'min ' + avgSeconds + 'sec';
-                }
-
-                return {
-                    name: `${index+1}. ${user.discordname}`,
-                    value: time + ' (' + avgTime + '/day)'
-                }
-            })
-        )
-        .setFooter({ text: 'You need to quit the voice channel to update the time!', iconURL: interaction.guild.iconURL() });
-        return { embeds: [leaderboardEmbed] }
-    } catch (error) {
-        logtail.error(error);
+    const date = new Date();
+    const startDate = parseInt(process.env.START_DATE) * 1000;
+    let difference = Math.round(
+      (date.getTime() - startDate) / (1000 * 3600 * 24),
+    );
+    if (difference === 0) {
+      difference = 1;
     }
+
+    const leaderboardEmbed = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setTitle(`${interaction.guild.name}'s leaderboard`)
+      .setThumbnail(interaction.guild.iconURL())
+      .setTimestamp()
+      .addFields(
+        users.map((user, index) => {
+          const hours = Math.floor(user.time / 3600);
+          const minutes = Math.floor((user.time % 3600) / 60);
+          const seconds = Math.floor(user.time % 60);
+          let time = "";
+
+          if (minutes === 0 && hours === 0) {
+            time = seconds + "sec";
+          } else if (hours === 0) {
+            time = minutes + "min " + seconds + "sec";
+          } else {
+            time = hours + "h " + minutes + "min " + seconds + "sec";
+          }
+
+          const avgHours = Math.floor(user.time / 3600 / difference);
+          const avgMinutes = Math.floor((user.time % 3600) / difference / 60);
+          const avgSeconds = Math.floor((user.time / difference) % 60);
+          let avgTime = "";
+
+          if (avgHours === 0 && avgMinutes === 0) {
+            avgTime = avgSeconds + "sec";
+          } else if (avgHours === 0) {
+            avgTime = avgMinutes + "min " + avgSeconds + "sec";
+          } else {
+            avgTime =
+              avgHours + "h " + avgMinutes + "min " + avgSeconds + "sec";
+          }
+
+          return {
+            name: `${index + 1}. ${user.discordname}`,
+            value: time + " (" + avgTime + "/day)",
+          };
+        }),
+      )
+      .setFooter({
+        text: "You need to quit the voice channel to update the time!",
+        iconURL: interaction.guild.iconURL(),
+      });
+    return { embeds: [leaderboardEmbed] };
+  } catch (error) {
+    logtail.error(error);
+  }
 }
 
 async function run(interaction) {
-    const response = await getLeaderboard(interaction);
-    if (response == null) {
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        return;
-    } else {
-        await interaction.reply(response);
-        logtail.info(`Leaderboard command executed by ${interaction.user.username}`);
-    }
+  const response = await getLeaderboard(interaction);
+  if (response == null) {
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+    return;
+  } else {
+    await interaction.reply(response);
+    logtail.info(
+      `Leaderboard command executed by ${interaction.user.username}`,
+    );
+  }
 }
 
-export { COMMAND_DEFINITION, run }
+export { COMMAND_DEFINITION, run };
--- src/commands/time.mjs
+++ src/commands/time.mjs
@@ -1,90 +1,111 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { getUser } from '../database.mjs';
-import { EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { getUser } from "../database.mjs";
+import { EmbedBuilder } from "discord.js";
 import { Logtail } from "@logtail/node";
 
 const logtail = new Logtail(process.env.SOURCE_TOKEN);
 
 let COMMAND_DEFINITION = new SlashCommandBuilder()
-    .setName('time')
-    .setDescription('Replies with the time you have spent in voice channels!');
+  .setName("time")
+  .setDescription("Replies with the time you have spent in voice channels!");
 
 async function getTime(interaction) {
-    try {
-        const startDate = parseInt(process.env.START_DATE)*1000;
-        const discordUser = interaction.user.username;
-        const databaseUser = await getUser(discordUser);
+  try {
+    const startDate = parseInt(process.env.START_DATE) * 1000;
+    const discordUser = interaction.user.username;
+    const databaseUser = await getUser(discordUser);
 
-        if (databaseUser === null) {
-            return 'You have not joined any voice channels yet!';
-        }
-
-        const lastJoined = new Date(parseInt(databaseUser.lastjoined));
-        const date = new Date();
-        let difference = Math.round((date.getTime() - startDate)/(1000*3600*24))
-
-        const hours = Math.floor(parseInt(databaseUser.time)/3600);
-        const minutes = Math.floor((parseInt(databaseUser.time)%3600)/60);
-        const seconds = parseInt(databaseUser.time)%60;
-        let time = "";
-
-        if (hours === 0) {
-            time = minutes + 'min ' + seconds + 'sec';
-        } else {
-            time = hours + 'h ' + minutes + 'min ' + seconds + 'sec';
-        }
-        
-        if (difference === 0) {
-            difference = 1;
-        }
-
-        const avgHours = Math.floor(parseInt(databaseUser.time)/3600/difference);
-        const avgMinutes = Math.floor((parseInt(databaseUser.time)%3600/difference)/60);
-        const avgSeconds = Math.floor((parseInt(databaseUser.time)/difference)%60);
-        let avgTime = "";
-
-        if (avgHours === 0) {
-            avgTime = avgMinutes + 'min ' + avgSeconds + 'sec';
-        } else {
-            avgTime = avgHours + 'h ' + avgMinutes + 'min ' + avgSeconds + 'sec';
-        }
-
-        const timeCommand = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(`${discordUser}'s stats`)
-        .setTimestamp()
-        .setThumbnail(interaction.user.avatarURL())
-        .addFields(
-            {
-                name: 'Time spent in voice channels',
-                value: time
-            },
-            {
-                TODO: 'Add the last time the user left a voice channel instead of the last time they joined one.',
-                name: `Last time ${discordUser} joined a voice channel`,
-                value: 'On ' + lastJoined.getDate() + '/' + lastJoined.getMonth() + '/' + lastJoined.getFullYear() + ' at ' + lastJoined.getHours() + ':' + lastJoined.getMinutes()
-            },
-            {
-                name: 'Average time spent in voice channels',
-                value: avgTime + '/day'
-            }
-        )
-        .setFooter({ text: 'You need to quit the voice channel to update the time!', iconURL: interaction.guild.iconURL() });
-        return { embeds: [timeCommand]}
-    } catch (error) {
-        logtail.error(error);
+    if (databaseUser === null) {
+      return "You have not joined any voice channels yet!";
     }
+
+    const lastJoined = new Date(databaseUser.lastjoined * 1000);
+    const date = new Date();
+    let difference = Math.round(
+      (date.getTime() - startDate) / (1000 * 3600 * 24),
+    );
+
+    const hours = Math.floor(databaseUser.time / 3600);
+    const minutes = Math.floor((databaseUser.time % 3600) / 60);
+    const seconds = Math.floor(databaseUser.time % 60);
+    let time = "";
+
+    if (minutes === 0 && hours === 0) {
+      time = seconds + "sec";
+    } else if (hours === 0) {
+      time = minutes + "min " + seconds + "sec";
+    } else {
+      time = hours + "h " + minutes + "min " + seconds + "sec";
+    }
+
+    if (difference === 0) {
+      difference = 1;
+    }
+
+    const avgHours = Math.floor(databaseUser.time / 3600 / difference);
+    const avgMinutes = Math.floor((databaseUser.time % 3600) / difference / 60);
+    const avgSeconds = Math.floor((databaseUser.time / difference) % 60);
+    let avgTime = "";
+
+    if (avgHours === 0 && avgMinutes === 0) {
+      avgTime = avgSeconds + "sec";
+    } else if (avgHours === 0) {
+      avgTime = avgMinutes + "min " + avgSeconds + "sec";
+    } else {
+      avgTime = avgHours + "h " + avgMinutes + "min " + avgSeconds + "sec";
+    }
+
+    const timeCommand = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setTitle(`${discordUser}'s stats`)
+      .setTimestamp()
+      .setThumbnail(interaction.user.avatarURL())
+      .addFields(
+        {
+          name: "Time spent in voice channels",
+          value: time,
+        },
+        {
+          name: `Last time ${discordUser} was in a voice channel`,
+          value:
+            "On " +
+            lastJoined.getDate() +
+            "/" +
+            String(parseInt(lastJoined.getMonth()) + 1) +
+            "/" +
+            lastJoined.getFullYear() +
+            " at " +
+            lastJoined.getHours() +
+            ":" +
+            lastJoined.getMinutes(),
+        },
+        {
+          name: "Average time spent in voice channels",
+          value: avgTime + "/day",
+        },
+      )
+      .setFooter({
+        text: "You need to quit the voice channel to update the time!",
+        iconURL: interaction.guild.iconURL(),
+      });
+    return { embeds: [timeCommand] };
+  } catch (error) {
+    logtail.error(error);
+  }
 }
 
 async function run(interaction) {
-    const response = await getTime(interaction);
-    if (response === null) {
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        return;
-    } else {
-        await interaction.reply(response);
-        logtail.info(`Time command executed by ${interaction.user.username}`);
-    }
+  const response = await getTime(interaction);
+  if (response === null) {
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+    return;
+  } else {
+    await interaction.reply(response);
+    logtail.info(`Time command executed by ${interaction.user.username}`);
+  }
 }
 
-export { COMMAND_DEFINITION, run }
+export { COMMAND_DEFINITION, run };
